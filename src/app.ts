b63961f52d7f40736b79d954c4b6df71b67e49a0
otash16 <otajonqutbiddinov1@gmail.com>
2025-08-30 02:15:35 +0500

    feat: app tes has been modified

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,105 @@
 
+// import cors from "cors";
+// import express from "express";
+// import path from "path";
+// import router from "./router";
+// import routerAdmin from "./router-admin";
+// import morgan from "morgan";
+// import cookieParser from "cookie-parser";
+// import { MORGAN_FORMAT } from "./libs/config";
+// import { T } from "./libs/types/common";
+// import { Server as SocketIOServer} from "socket.io"
+// import http from "http"
+
+// import session from "express-session";
+// import ConnectMongDB from "connect-mongodb-session";
+
+// const MongoDBStore = ConnectMongDB(session);
+
+// const store = new MongoDBStore({
+//   uri: String(process.env.MONGO_URL),
+//   collection: "sessions",
+// });
+
+// // 1-ENTRANCE
+// const app = express();
+// // console.log(__dirname);
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use("/uploads", express.static("./uploads"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // app.use(cors({ credentials: true, origin: true }));
+// app.use(
+//   cors({
+//     origin: "http://glamour-resto.uz", // production frontend
+//     credentials: true,
+//   })
+// );
+// app.use(cookieParser());
+// app.use(morgan(MORGAN_FORMAT));
+
+// // 2- SESSIONS
+// app.use(
+//   session({
+//     secret: String(process.env.SESSION_SECRET),
+//     cookie: {
+//       maxAge: 1000 * 3600 * 6, // 3h
+//       httpOnly: true,
+//     },
+//     store: store,
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
+
+
+// app.use(function (req, res, next) {
+//   const sessionInstance = req.session as T;
+//   res.locals.member = sessionInstance.member;
+//   next();
+// });
+
+// // 3- VIEWS
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+
+// // 4- ROUTERS
+// app.use("/admin", routerAdmin); // SSR: EJS
+// app.use("/", router); // SPA: REACT
+
+// const server = http.createServer(app);
+// const io = new SocketIOServer(server, {
+//   cors: {
+//     origin: true,
+//     credentials: true,
+//   },
+// })
+
+// let summaryClient = 0;
+// io.on("connection", (socket) => {
+//   summaryClient++;
+//   console.log(`Connection $ total [${summaryClient}]`);
+
+//   socket.on("disconnect", () => {
+//     summaryClient--;
+//     console.log(`Disconnection $ total [${summaryClient}]`);
+//   })
+// })
+
+// export default server;
 import cors from "cors";
 import express from "express";
 import path from "path";
 import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
-import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
-import { T } from "./libs/types/common";
-import { Server as SocketIOServer} from "socket.io"
-import http from "http"
-
+import cookieParser from "cookie-parser";
 import session from "express-session";
 import ConnectMongDB from "connect-mongodb-session";
+import { T } from "./libs/types/common";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
 
 const MongoDBStore = ConnectMongDB(session);
 
@@ -38,14 +125,12 @@ app.use(
     secret: String(process.env.SESSION_SECRET),
     cookie: {
       maxAge: 1000 * 3600 * 6, // 3h
-      httpOnly: true,
     },
     store: store,
     resave: true,
     saveUninitialized: true,
   })
 );
-
 
 app.use(function (req, res, next) {
   const sessionInstance = req.session as T;
@@ -67,17 +152,17 @@ const io = new SocketIOServer(server, {
     origin: true,
     credentials: true,
   },
-})
+});
 
 let summaryClient = 0;
 io.on("connection", (socket) => {
   summaryClient++;
-  console.log(`Connection $ total [${summaryClient}]`);
+  console.log(`Connection & total [${summaryClient}]`);
 
   socket.on("disconnect", () => {
     summaryClient--;
-    console.log(`Disconnection $ total [${summaryClient}]`);
-  })
-})
+    console.log(`Disonnection & total [${summaryClient}]`);
+  });
+});
 
 export default server;
