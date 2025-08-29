4ef211847d0c94c16d630e73bad80495ffc5d66b
otash16 <otajonqutbiddinov1@gmail.com>
2025-08-30 02:30:25 +0500

    feat: server ts has been sacrifised

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,3 @@
-// import dotenv from 'dotenv';
-// dotenv.config({
-//     path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
-// });
-
-// import mongoose from 'mongoose';
-// import server from './app';
-// mongoose.connect(process.env.MONGO_URL as string, {})
-// .then((data) => {
-//     console.log("MongoDB connection succeed");
-//     const PORT = process.env.PORT ?? 3003;
-//     server.listen(PORT, function() {
-//         console.log(`The server is running successfully on port: ${PORT}`);
-//     });
-// })
-// .catch(err => console.log('ERROR on connection MongoDB', err));
 import dotenv from 'dotenv';
 dotenv.config({
     path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
@@ -21,16 +5,12 @@ dotenv.config({
 
 import mongoose from 'mongoose';
 import server from './app';
-
 mongoose.connect(process.env.MONGO_URL as string, {})
 .then((data) => {
     console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3003;
-
-// IPv4 va IPv6 uchun
-    server.listen(PORT, '0.0.0.0' as any, () => {
-    console.log(`The server is running successfully on port: ${PORT}`);
-});
-
+    const PORT = process.env.PORT ?? 3003;
+    server.listen(PORT, function() {
+        console.log(`The server is running successfully on port: ${PORT}`);
+    });
 })
 .catch(err => console.log('ERROR on connection MongoDB', err));
